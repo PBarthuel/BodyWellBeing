0e434259b081c0b44b46c3a96efe4b5352ad15b9
Barthuel <paul.barthuel@withings.com>
2022-02-06 17:24:48 +0100

    Adding constants for exercices

---
+++ app/src/main/java/com/pbarthuel/bodywellbeing/data/constants/ExercisesConstants.kt
@@ -0,0 +1,11 @@
+package com.pbarthuel.bodywellbeing.data.constants
+
+object ExercisesConstants {
+
+    const val ARM_EXERCISE_TYPE: Int = 1
+    const val TRICEPS_EXERCISE_TYPE: Int = 2
+    const val BACK_EXERCISE_TYPE: Int = 3
+    const val SHOULDER_EXERCISE_TYPE: Int = 4
+    const val ABS_EXERCISE_TYPE: Int = 5
+    const val LEG_EXERCISE_TYPE: Int = 6
+}
