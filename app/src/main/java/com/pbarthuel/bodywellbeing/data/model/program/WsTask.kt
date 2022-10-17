e458d0d561a0719d6e8f908008c04af3aeeac9ff
Paul Barthuel <pbarthuel@dhcp-13-233.corp.withings.com>
2022-10-17 15:36:53 +0200

    preparing program join

--- app/src/main/java/com/pbarthuel/bodywellbeing/data/model/program/WsTask.kt
+++ app/src/main/java/com/pbarthuel/bodywellbeing/data/model/program/WsTask.kt
@@ -6,5 +6,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class WsTask(
     @SerializedName("id") val id: String = "",
-    @SerializedName("type") val type: String = ""
+    @SerializedName("title") val title: String = "",
+    @SerializedName("thumbnail") val thumbnail: String = "",
+    @SerializedName("type") val type: Int = 0
 )
--- app/src/main/java/com/pbarthuel/bodywellbeing/data/vendors/local/room/programs/program/entities/TaskEntity.kt
+++ app/src/main/java/com/pbarthuel/bodywellbeing/data/vendors/local/room/programs/program/entities/TaskEntity.kt
@@ -10,11 +10,15 @@ import kotlinx.serialization.Serializable
 data class TaskEntity(
     @ColumnInfo(name = "localId")
     @PrimaryKey(autoGenerate = true)
-    val localId: Int,
+    val localId: Int, // I need the localId to be auto generated to avoid conflict because the same task can be duplicated in a program
     @ColumnInfo(name = "id")
     val id: String,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "thumbnail")
+    val thumbnail: String,
     @ColumnInfo(name = "dayIndex")
     val dayIndex: Int,
     @ColumnInfo(name = "type")
-    val type: String
+    val type: Int
 )
